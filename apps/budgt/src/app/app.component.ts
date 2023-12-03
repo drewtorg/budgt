4e73050ad9e92317225d01b1ed921561b339865f
Drew Torgeson <drew.torgeson@gmail.com>
2023-12-03 12:31:36 +0100

    feat: add connection to firebase

--- apps/budgt/src/app/app.component.ts
+++ apps/budgt/src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { AsyncPipe, JsonPipe, NgFor } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, AsyncPipe, JsonPipe, NgFor],
   selector: 'budgt-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'budgt';
+
+  firestore = inject(Firestore);
+
+  budgets$ = collectionData(collection(this.firestore, 'budget'));
 }
--- apps/budgt/src/app/app.config.ts
+++ apps/budgt/src/app/app.config.ts
@@ -1,7 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          apiKey: 'AIzaSyCCvQZ3FZ6AIb2olIUuVf9wbxvuUOZgU6g',
+          authDomain: 'budgt-3ecb0.firebaseapp.com',
+          databaseURL: 'https://budgt-3ecb0.firebaseio.com',
+          projectId: 'budgt-3ecb0',
+        }),
+      ),
+      provideFirestore(() => getFirestore()),
+      provideAuth(() => getAuth()),
+    ),
+  ],
 };
