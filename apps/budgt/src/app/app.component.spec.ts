ba9a506cc33a12c0150fd4252d9134855b91790e
Drew Torgeson <drew.torgeson@gmail.com>
2023-12-03 13:17:07 +0100

    chore: cleanup all exra cruft

--- apps/budgt/src/app/app.component.spec.ts
+++ apps/budgt/src/app/app.component.spec.ts
@@ -1,27 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent],
     }).compileComponents();
+
+    component = TestBed.inject(AppComponent);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome budgt'
-    );
-  });
-
-  it(`should have as title 'budgt'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('budgt');
+  it('should create', () => {
+    expect(component).toBeDefined();
   });
 });
--- apps/budgt/src/app/app.component.ts
+++ apps/budgt/src/app/app.component.ts
@@ -1,20 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { AsyncPipe, JsonPipe, NgFor } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, AsyncPipe, JsonPipe, NgFor],
+  imports: [RouterModule],
   selector: 'budgt-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'budgt';
-
-  firestore = inject(Firestore);
-
-  budgets$ = collectionData(collection(this.firestore, 'budget'));
-}
+export class AppComponent {}
