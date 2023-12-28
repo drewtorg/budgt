192c9c912e15b65233b0623a3e484d0af745faa5
Drew Torgeson <drew.torgeson@gmail.com>
2023-12-28 15:39:01 +0100

    fix: ensure expense date is calculated correctly

--- libs/shared/functions/src/lib/functions/date.ts
+++ libs/shared/functions/src/lib/functions/date.ts
@@ -1,3 +1,10 @@
 export function dateToString(date: Date) {
-  return date.toISOString().split('T')[0];
+  let month = '' + (date.getMonth() + 1);
+  let day = '' + date.getDate();
+  const year = date.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
 }
