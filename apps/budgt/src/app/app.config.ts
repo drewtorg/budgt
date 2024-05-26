7884ab87c5015b2a1dc17a398f4ce3b68d6208ea
Drew <drew@Drews-MacBook-Pro.local>
2024-05-26 21:08:30 +0200

    fix(deps): update dependency @angular/fire to v17.1.0

--- apps/budgt/src/app/app.config.ts
+++ apps/budgt/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -6,28 +10,33 @@ import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withComponentInputBinding()),
-    importProvidersFrom(MatNativeDateModule, provideFirebaseApp(() => initializeApp({
+    provideFirebaseApp(() =>
+      initializeApp({
         apiKey: 'AIzaSyCCvQZ3FZ6AIb2olIUuVf9wbxvuUOZgU6g',
         authDomain: 'budgt-3ecb0.firebaseapp.com',
         databaseURL: 'https://budgt-3ecb0.firebaseio.com',
         projectId: 'budgt-3ecb0',
-    })), provideFirestore(() => getFirestore()), provideAuth(() => getAuth())),
+      }),
+    ),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    importProvidersFrom(MatNativeDateModule),
     { provide: MAT_DATE_LOCALE, useValue: 'sv-SE' },
     provideAnimations(),
     {
-        provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-        useValue: {
-            hideRequiredMarker: true,
-        },
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        hideRequiredMarker: true,
+      },
     },
     provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })
-],
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
